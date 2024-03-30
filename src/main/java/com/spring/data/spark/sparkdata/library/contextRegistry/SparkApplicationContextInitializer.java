09cd543c073c7386a398dbf3d0d4d062e4d124b2
Zaby2 <work.acca@yandex.ru>
2024-03-30 12:37:51 +0300

    Spark init almost completed

--- src/main/java/com/spring/data/spark/sparkdata/library/contextRegistry/SparkApplicationContextInitializer.java
+++ src/main/java/com/spring/data/spark/sparkdata/library/contextRegistry/SparkApplicationContextInitializer.java
@@ -1,18 +1,40 @@
 package com.spring.data.spark.sparkdata.library.contextRegistry;
 
+import com.spring.data.spark.sparkdata.library.repository.SparkRepository;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import org.reflections.Reflections;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.beans.Introspector;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 public class SparkApplicationContextInitializer implements ApplicationContextInitializer {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) { // now this context is almost empty
         registerSparkBean(applicationContext);
-        
+        String scannerPath = applicationContext.getEnvironment().getProperty("spark.packages-to-scan");
+        Reflections scanner = new Reflections(scannerPath);
+        Set<Class<? extends SparkRepository>> sparkRepoImpls = scanner.getSubTypesOf(SparkRepository.class);
+
+        sparkRepoImpls.forEach(sparkRepoImpl -> {
+            Object newProxyInstance = Proxy.newProxyInstance(sparkRepoImpl.getClassLoader(), new Class[]{SparkRepository.class}, ih);
+            applicationContext.getBeanFactory().registerSingleton(Introspector.decapitalize(sparkRepoImpl.getSimpleName()), newProxyInstance); // need to create annd set
+        });
+
     }
 
+
+    // Later need to add params that are required during spark configuration
     private void registerSparkBean(ConfigurableApplicationContext applicationContext) {
-        SparkSession.builder().appName("null").master("local[*]").getOrCreate(); // need to get this params from env
+        String appName = applicationContext.getEnvironment().getProperty("spark.app-name");
+        SparkSession sparkSession = SparkSession.builder().appName(appName).master("local[*]").getOrCreate(); // need to create beans that will be used based on profile
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkSession.sparkContext());
+        applicationContext.getBeanFactory().registerSingleton("sparkContext", sparkContext);
+        applicationContext.getBeanFactory().registerSingleton("sparkSession", sparkSession);
     }
 }
--- src/main/java/com/spring/data/spark/sparkdata/library/props/SparkPropsHandler.java
+++ src/main/java/com/spring/data/spark/sparkdata/library/props/SparkPropsHandler.java
@@ -1,9 +1,17 @@
 package com.spring.data.spark.sparkdata.library.props;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 // this class is used for auto - complete idea training in application properties
 @ConfigurationProperties(prefix = "spark")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SparkPropsHandler {
     private String appName;
     private String packagesToScan;
